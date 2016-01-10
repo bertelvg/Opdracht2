0225e0e0d589e199e7dc6afebcfe4dcc81eb8be1
BertelVanGansbeke <bertel.van.gansbeke@gmail.com>
2016-01-10 14:21:02 +0100

    comments

--- wordpress/wp-content/plugins/opdracht3/index.php
+++ wordpress/wp-content/plugins/opdracht3/index.php
@@ -15,11 +15,34 @@
 */
 
 
-// create custom plugin settings menu
+// DEEL 1: ADD MENU ITEM + SUBMENU
+
+// 1.1 MENU AANMAKEN 
+add_action('admin_menu', 'sep_menuexample_create_menu' );
+function sep_menuexample_create_menu() {
+    add_menu_page( 'My Plugin Settings Page', 'Opdracht 3','manage_options', __FILE__, 'sep_menuexample_settings_page',screen_icon('edit'));
+    add_submenu_page( __FILE__, 'Settings opdracht 3','Settings', 'manage_options',__FILE__.'_menu1', 'my_cool_plugin_settings_page' );
+    add_action( 'admin_init', 'register_my_cool_plugin_settings' );
+}
 
 
+// 1.2 OUTPUT HOOFDPAGINA PLUGIN
+function sep_menuexample_settings_page(){?>
+   
+    <div class="wrap">
+        <h2>Opdracht 3</h2>
+        <p> (Settings in submenu)</p>
+        <p> Werkstuk opracht 3 Advanced web development</p>
+        <a href="http://www.erasmushogeschool.be/opleidingen/bachelors/multimedia-communicatietechnologie-multec">Erasmus hogeschool brussel // multec</a>
+        <br><br><hr><a href="http://bertelvangansbeke.be/#1">Bertel Van Gansbeke</a> <p>3e Bach Multec App and web</p>
+    </div>
+
+<?php }
 
 
+// DEEL 2: 3 PLUGIN SETTINGS
+
+// 2.1 REGISTREREN VAN SETTINGS
 function register_my_cool_plugin_settings() {
     //register our settings
     register_setting( 'my-cool-plugin-settings-group', 'font-size' );
@@ -27,69 +50,62 @@ function register_my_cool_plugin_settings() {
     register_setting( 'my-cool-plugin-settings-group', 'font-weight' );
 }
 
+// 2.2 OUTPUT SETTINGSPAGINA (SUBMENU) PLUGIN
 function my_cool_plugin_settings_page() {
 ?>
-<div class="wrap">
-
-    
-<h2>Opdracht 3: settings vakgebied</h2>
-
-<form method="post" action="options.php">
-    <?php settings_fields( 'my-cool-plugin-settings-group' ); ?>
-    <?php do_settings_sections( 'my-cool-plugin-settings-group' ); ?>
-    <table class="form-table">
-        <tr valign="top">
-        <th scope="row">Font-size (px)</th>
-        <td><input type="text" name="font-size" value="<?php echo esc_attr( get_option('font-size') ); ?>" /></td>
-        </tr>
-         
-        <tr valign="top">
-        <th scope="row">Font-color (hex)</th>
-        <td><input type="text" name="font-color" value="<?php echo esc_attr( get_option('font-color') ); ?>" /></td>
-        </tr>
+    <div class="wrap">
         
-        <tr valign="top">
-        <th scope="row">Font-weight (100-900)</th>
-        <td><input type="text" name="font-weight" value="<?php echo esc_attr( get_option('font-weight') ); ?>" /></td>
-        </tr>
-
-
-    </table>
-    
-    <?php submit_button(); ?>
-
-</form>
-</div>
+        <h2>Opdracht 3: settings vakgebied</h2>
+            <form method="post" action="options.php">
+                <?php settings_fields( 'my-cool-plugin-settings-group' ); ?>
+                <?php do_settings_sections( 'my-cool-plugin-settings-group' ); ?>
+                <table class="form-table">
+                    <tr valign="top">
+                        <th scope="row">Font-size (px)</th>
+                        <td><input type="text" name="font-size" value="<?php echo esc_attr( get_option('font-size') ); ?>" /></td>
+                    </tr>
+                     
+                    <tr valign="top">
+                        <th scope="row">Font-color (hex)</th>
+                        <td><input type="text" name="font-color" value="<?php echo esc_attr( get_option('font-color') ); ?>" /></td>
+                    </tr>
+                    
+                    <tr valign="top">
+                        <th scope="row">Font-weight (100-900)</th>
+                        <td><input type="text" name="font-weight" value="<?php echo esc_attr( get_option('font-weight') ); ?>" /></td>
+                    </tr>
+                </table>         
+                <?php submit_button(); ?>
+            </form>
+    </div>
 <?php }
 
 
-/**
- * METABOX! 
- */
+// DEEL 3: METABOX AAN POST PAGINA TOEVOGEN + VERWERKEN
+
+// 3.1 AANMAKEN METABOX OP POST PAGE
 function prfx_custom_meta() {
     add_meta_box( 'prfx_meta', __( 'VAKGEBIED', 'prfx-textdomain' ), 'prfx_meta_callback', 'post' );
 }
 add_action( 'add_meta_boxes', 'prfx_custom_meta' );
 
-/**
- * Outputs the content of the meta box
- */
+
+// 3.2 OUTPUT VAN METABOX OP POST PAGE
 function prfx_meta_callback( $post ) {
     wp_nonce_field( basename( __FILE__ ), 'prfx_nonce' );
     $prfx_stored_meta = get_post_meta( $post->ID );
     ?>
  
-    <p>
-        <label for="meta-text" class="prfx-row-title"><?php _e( 'Vakgebied:', 'prfx-textdomain' )?></label>
-        <input type="text" name="meta-text" id="meta-text" value="<?php if ( isset ( $prfx_stored_meta['meta-text'] ) ) echo $prfx_stored_meta['meta-text'][0]; ?>" />
-    </p>
+        <p>
+            <label for="meta-text" class="prfx-row-title"><?php _e( 'Vakgebied:', 'prfx-textdomain' )?></label>
+            <input type="text" name="meta-text" id="meta-text" value="<?php if ( isset ( $prfx_stored_meta['meta-text'] ) ) echo $prfx_stored_meta['meta-text'][0]; ?>" />
+        </p>
  
     <?php
 }
 
-/**
- * Saves the custom meta input
- */
+// 3.2 OPSLAAN VAN METABOX INPUT 
+
 function prfx_meta_save( $post_id ) {
  
     // Checks save status
@@ -117,29 +133,7 @@ add_action( 'save_post', 'prfx_meta_save' );
 
 
 
-<?php
- 
-add_action('admin_menu', 'sep_menuexample_create_menu' );
-function sep_menuexample_create_menu() {
-//create custom top-level menu
-add_menu_page( 'My Plugin Settings Page', 'Opdracht 3','manage_options', __FILE__, 'sep_menuexample_settings_page',screen_icon('edit'));
 
-add_submenu_page( __FILE__, 'Settings opdracht 3','Settings', 'manage_options',__FILE__.'_menu1', 'my_cool_plugin_settings_page' );
-add_action( 'admin_init', 'register_my_cool_plugin_settings' );
-}
-function sep_menuexample_settings_page(){?>
-   
-<div class="wrap">
-
-    
-<h2>Opdracht 3</h2>
-<p> (Settings in submenu)</p>
-<p> Werkstuk opracht 3 Advanced web development</p>
-<a href="http://www.erasmushogeschool.be/opleidingen/bachelors/multimedia-communicatietechnologie-multec">Erasmus hogeschool brussel // multec</a>
-<br><br><hr><a href="http://bertelvangansbeke.be/#1">Bertel Van Gansbeke</a> <p>3e Bach Multec App and web</p>
-</div>
-
-<?php }
 
 
 
--- wordpress/wp-content/themes/foundation/content-blog.php
+++ wordpress/wp-content/themes/foundation/content-blog.php
@@ -10,12 +10,13 @@
     <!--Title opvragen-->
     <h3 class="margin20" ><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h3>
     <!--Autheur en datum opvragen-->
-    <h6>Written by <?php the_author(); ?> on <?php the_time('F j, Y');?> // course: 
+    <!--CONTENT UIT PLUGIN METABOX EN SETTINGS VERWERKEN-->
+    <h6>Written by <?php the_author(); ?> on <?php the_time('F j, Y');?> // course: <span style="color:<?php echo get_settings('font-color'); ?>; font-size:<?php echo get_settings('font-size'); ?>px; font-weight:<?php echo get_settings('font-weight'); ?>; ">
       <?php $meta_value = get_post_meta( get_the_ID(), 'meta-text', true );
       if( !empty( $meta_value ) ) {
           echo $meta_value;
       }
-      ?>
+      ?></span>  
     </h6>
         
     <div class="row">        
